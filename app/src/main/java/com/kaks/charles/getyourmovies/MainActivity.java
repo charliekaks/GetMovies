ff71403712adab378ef417d310d6aa0ae73858f6
charliekaks <kaksunchained@gmail.com>
2017-09-08 15:21:51 +0300

    intial commit

---
+++ app/src/main/java/com/kaks/charles/getyourmovies/MainActivity.java
@@ -0,0 +1,47 @@
+package com.kaks.charles.getyourmovies;
+
+import android.content.Intent;
+import android.graphics.Movie;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.movieButton) Button mMovieButton;
+    @Bind(R.id.docButton) Button mDocButton;
+    @Bind(R.id.logButton) Button mLogButton;
+    @Bind(R.id.tvButton) Button mTvButton;
+    @Bind(R.id.signButton)  Button mSignButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mMovieButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, movies.class);
+                startActivity(intent);
+            }
+        });
+        mDocButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Docs.class);
+                startActivity(intent);
+            }
+        });
+        mTvButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Tv.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/kaks/charles/getyourmovies/movies.java
@@ -0,0 +1,13 @@
+package com.kaks.charles.getyourmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class movies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
