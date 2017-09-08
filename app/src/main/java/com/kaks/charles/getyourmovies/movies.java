5ae25d8bdfa0faf62b4725f0822f92e901c0d610
charliekaks <kaksunchained@gmail.com>
2017-09-08 16:33:59 +0300

    made changes to the movie.java; added an array adapter to implement a grid view

--- app/src/main/java/com/kaks/charles/getyourmovies/movies.java
+++ app/src/main/java/com/kaks/charles/getyourmovies/movies.java
@@ -2,12 +2,18 @@ package com.kaks.charles.getyourmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class movies extends AppCompatActivity {
-
+    @Bind(R.id.movieGrid) GridView mMovieGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        ButterKnife.bind(this);
+        mMovieGrid.setAdapter(new ImageAdapter(this));
     }
 }
