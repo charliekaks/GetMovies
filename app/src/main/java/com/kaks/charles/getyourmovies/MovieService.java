c2808c59b936754d15bab78ce5b2fa0738ea4268
charliekaks <kaksunchained@gmail.com>
2017-09-15 08:14:47 +0300

    Adding Methods to process results from the json; The method includes the processResults that parses json data

---
+++ app/src/main/java/com/kaks/charles/getyourmovies/MovieService.java
@@ -0,0 +1,89 @@
+package com.kaks.charles.getyourmovies;
+
+import android.util.Log;
+
+import com.kaks.charles.getyourmovies.models.MovieModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by charles on 9/14/17.
+ */
+
+public class MovieService {
+    public static void findMovie(String category, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL_POPULAR).newBuilder();
+                                    urlBuilder.addQueryParameter( Constants.API_KEY, Constants.MOVIE_API_KEY)
+                                            .addQueryParameter(Constants.QUERY,category);
+
+        String url = urlBuilder.build().toString();
+        Log.v("THE URL", url);
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public static void findPopularMovies(Callback callback){
+        OkHttpClient clientMovie = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+                                    urlBuilder.addPathSegments("popular")
+                                    .addQueryParameter(Constants.API_KEY,Constants.MOVIE_API_KEY);
+
+        String url = urlBuilder.build().toString();
+        Log.v("THE Popular URL", url);
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = clientMovie.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public ArrayList<MovieModel> processMovies(Response response){
+        ArrayList<MovieModel> movies = new ArrayList<>();
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()){
+                JSONObject movieJSON = new JSONObject(jsonData);
+                JSONArray resultJSON = movieJSON.getJSONArray("results");
+                for (int i = 0; i <resultJSON.length() ; i++) {
+                    JSONObject movieResults = resultJSON.getJSONObject(i);
+                    String overview = movieResults.getString("overview");
+                    String original_title = movieResults.getString("original_title");
+                    String title = movieResults.getString("title");
+                    String imageUrl = movieResults.getString("poster_path");
+                    String release = movieResults.getString("release_date");
+                    Integer voter_average = movieResults.getInt("vote_average");
+                    Integer popularity = movieResults.getInt("popularity");
+                    MovieModel mMovies = new MovieModel(overview,original_title,title,imageUrl,release,voter_average,popularity);
+                    movies.add(mMovies);
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+}
