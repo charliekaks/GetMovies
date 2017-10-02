f93fb5f737dc0b5f603ff2e347617bf23f0c3b36
charliekaks <kaksunchained@gmail.com>
2017-10-02 10:21:17 +0300

    Updated the Constants to include an index for when the items are moved

--- app/src/main/java/com/kaks/charles/getyourmovies/Constants.java
+++ app/src/main/java/com/kaks/charles/getyourmovies/Constants.java
@@ -3,8 +3,6 @@ package com.kaks.charles.getyourmovies;
 /**
  * Created by charles on 9/14/17.
  */
-//api.themoviedb.org/3/search/multi?api_key=a29a067555d7139438f64f51b33a2c69&language=en-US&query=shrek
-//api.themoviedb.org/3/multi?api_key=a29a067555d7139438f64f51b33a2c69&query=shrek
 
 public class Constants {
     public static final String MOVIE_API_KEY = BuildConfig.MOVIE_API_KEY;
@@ -15,5 +13,6 @@ public class Constants {
     public static final String POPULAR_MOVIES_FIREBASE = "popularMovies";
     public static final String FIREBASE_CHILD_SEARCHED_MOVIE = "searchedMovie";
     public static final String FIREBASE_CHILD_POPULAR_MOVIES = "popular";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 
 }
