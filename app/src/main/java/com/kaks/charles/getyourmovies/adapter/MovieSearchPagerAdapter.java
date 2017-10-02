f6af4233024a9b157aa25bfaf51e36a7ffb23f2d
charliekaks <kaksunchained@gmail.com>
2017-10-02 07:38:01 +0300

    Made changes to the Search pager adapter

---
+++ app/src/main/java/com/kaks/charles/getyourmovies/adapter/MovieSearchPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.kaks.charles.getyourmovies.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.kaks.charles.getyourmovies.SearchFragment;
+import com.kaks.charles.getyourmovies.models.MovieSearch;
+
+import java.util.ArrayList;
+
+/**
+ * Created by charles on 9/25/17.
+ */
+
+public class MovieSearchPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<MovieSearch> mMovies;
+
+    public MovieSearchPagerAdapter (FragmentManager fm, ArrayList<MovieSearch> movies) {
+        super(fm);
+        mMovies = movies;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return SearchFragment.newInstance(mMovies.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mMovies.size();
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mMovies.get(position).getTitle();
+    }
+}
