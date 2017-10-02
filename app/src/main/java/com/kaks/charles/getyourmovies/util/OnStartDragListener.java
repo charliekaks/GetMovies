49390c7bda23040adbb40d811acb5d76d2f9b2cf
charliekaks <kaksunchained@gmail.com>
2017-10-02 07:29:59 +0300

    Specific interface for the drag and drop functionality.

---
+++ app/src/main/java/com/kaks/charles/getyourmovies/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.kaks.charles.getyourmovies.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by charles on 9/29/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
